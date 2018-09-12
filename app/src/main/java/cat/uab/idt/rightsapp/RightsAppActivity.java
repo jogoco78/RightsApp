83990652f7dfdc5e5c206c697f3c8e069fc22a2a
Jorge Gonzalez <jogoco.idt@gmail.com>
2018-09-12 18:30:25 +0200

    Initial commit

---
+++ app/src/main/java/cat/uab/idt/rightsapp/RightsAppActivity.java
@@ -0,0 +1,13 @@
+package cat.uab.idt.rightsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RightsAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rights_app);
+    }
+}
