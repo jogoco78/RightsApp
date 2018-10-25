081bfe577af1d06ba9d83a57fc223293a88a9abd
Jorge Gonzalez <jogoco.idt@gmail.com>
2018-10-25 09:43:25 +0200

    RightsApp activity and Action Bar

--- app/src/main/java/cat/uab/idt/rightsapp/RightsAppActivity.java
+++ app/src/main/java/cat/uab/idt/rightsapp/RightsAppActivity.java
@@ -1,13 +1,11 @@
 package cat.uab.idt.rightsapp;
 
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.support.v7.widget.Toolbar;
 import android.widget.Toast;
