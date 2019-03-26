fd6bb6c52d7aefa1cadb41ec7e027f400cbcc45a
Jorge <jogoco.idt@gmail.com>
2019-03-26 16:17:58 +0100

    Some changes

--- app/src/main/java/cat/uab/idt/rightsapp/Emergency112CallActivity.java
+++ app/src/main/java/cat/uab/idt/rightsapp/Emergency112CallActivity.java
@@ -2,12 +2,20 @@ package cat.uab.idt.rightsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class Emergency112CallActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle(R.string.title_activity_call112);
         setContentView(R.layout.activity_emergency112_call);
+
+        //Sets the toolbar
+        Toolbar toolbarRightsApp = findViewById(R.id.toolbar_rights_app);
+        setSupportActionBar(toolbarRightsApp);
+
+
     }
 }
