c55bd65e7b4b90e56ce85621e247a7db150fc127
Jorge <jogoco.idt@gmail.com>
2020-05-21 10:39:10 +0200

    Version code 8, version name 1.6

--- app/src/main/java/cat/uab/idt/rightsapp/ExplanationActivity.java
+++ app/src/main/java/cat/uab/idt/rightsapp/ExplanationActivity.java
@@ -7,19 +7,27 @@ import android.content.SharedPreferences;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Typeface;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ExplanationActivity extends AppCompatActivity {
 
     private boolean showExplanation = true;
-    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +39,108 @@ public class ExplanationActivity extends AppCompatActivity {
         setSupportActionBar(toolbarRightsApp);
 
         final Switch sw_explanation = findViewById(R.id.switch_explanation);
-        //TextView tv_explanation = findViewById(R.id.tv_explanation_preferences_title);
-        //tv_explanation.setMovementMethod(new ScrollingMovementMethod());
+        TextView tv_howto_title = findViewById(R.id.tv_howto_title);
+        TextView tv_howto_body = findViewById(R.id.tv_howto_body);
+        TextView tv_howto_first = findViewById(R.id.tv_howto_first);
+        TextView tv_howto_second = findViewById(R.id.tv_howto_second);
+        TextView tv_howto_third = findViewById(R.id.tv_howto_third);
+        TextView tv_howto_preferences_title = findViewById(R.id.tv_howto_preferences_title);
+        TextView tv_howto_preferences_body = findViewById(R.id.tv_howto_preferences_body);
+        TextView tv_howto_preferences_first = findViewById(R.id.tv_howto_preferences_first);
+        TextView tv_howto_preferences_second = findViewById(R.id.tv_howto_preference_second);
+        TextView tv_howto_preferences_third = findViewById(R.id.tv_howto_preferences_third);
+        TextView tv_howto_preferences_fourth = findViewById(R.id.tv_howto_preferences_fourth);
+
+        int flag = Spannable.SPAN_EXCLUSIVE_EXCLUSIVE;
+
+        SpannableStringBuilder ssb_title = new SpannableStringBuilder();
+        SpannableStringBuilder ssb = new SpannableStringBuilder();
+
+        //HowTo title
+        ssb_title.clear();
+        ssb_title.append(getResources().getString(R.string.explanation_howTo_title));
+        ssb_title.setSpan (new StyleSpan(Typeface.BOLD), 0, ssb_title.length(), flag);
+        tv_howto_title.setText(ssb_title, TextView.BufferType.SPANNABLE);
+        tv_howto_title.setTextSize((float) 25.0);
+
+        //HowTo body
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_howTo_body));
+        tv_howto_body.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_body.setTextSize((float) 18.0);
+
+        //HowTo first scenario
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_howTo_first));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_howTo_first).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_howTo_first).indexOf(":"), flag);
+        tv_howto_first.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_first.setTextSize((float) 18.0);
+
+        //HowTo second scenario
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_howTo_second));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_howTo_second).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_howTo_second).indexOf(":"), flag);
+        tv_howto_second.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_second.setTextSize((float) 18.0);
+
+        //HowTo third scenario
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_howTo_third));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_howTo_third).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_howTo_third).indexOf(":"), flag);
+        tv_howto_third.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_third.setTextSize((float) 18.0);
+
+        //Preferences menu title
+        ssb_title.clear();
+        ssb_title.append(getResources().getString(R.string.explanation_preferences_title));
+        ssb_title.setSpan (new StyleSpan(Typeface.BOLD), 0, ssb_title.length(), flag);
+        tv_howto_preferences_title.setText(ssb_title, TextView.BufferType.SPANNABLE);
+        tv_howto_preferences_title.setTextSize((float) 25.0);
+
+        //Preferences body
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_preferences_body));
+        tv_howto_preferences_body.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_preferences_body.setTextSize((float) 18.0);
+
+        //First option preferences
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_preferences_first_scenario));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_preferences_first_scenario).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_preferences_first_scenario).indexOf(":"), flag);
+        tv_howto_preferences_first.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_preferences_first.setTextSize((float) 18.0);
+
+        //Second option preferences
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_preferences_second_scenario));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_preferences_second_scenario).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_preferences_second_scenario).indexOf(":"), flag);
+        tv_howto_preferences_second.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_preferences_second.setTextSize((float) 18.0);
+
+        //Third option preferences
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_preferences_third_scenario));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_preferences_third_scenario).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_preferences_third_scenario).indexOf(":"), flag);
+        tv_howto_preferences_third.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_preferences_third.setTextSize((float) 18.0);
+
+        //Fourth option preferences
+        ssb.clear();
+        ssb.append(getResources().getString(R.string.explanation_preferences_fourth_scenario));
+        ssb.setSpan(new UnderlineSpan(), 0, getResources().getString(R.string.explanation_preferences_fourth_scenario).indexOf(":"), flag);
+        ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, getResources().getString(R.string.explanation_preferences_fourth_scenario).indexOf(":"), flag);
+        tv_howto_preferences_fourth.setText(ssb, TextView.BufferType.SPANNABLE);
+        tv_howto_preferences_fourth.setTextSize((float) 18.0);
 
         // Gets preferences file
         Context context = getApplicationContext();
-        sharedPreferences = context.getSharedPreferences(
+        SharedPreferences sharedPreferences = context.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
         showExplanation = sharedPreferences.getBoolean(Constants.SHOW_EXPLANATION, true);
